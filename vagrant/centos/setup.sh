9f8137881edb2420e94520b90d678ce14c8967d0
SatoShun <shun.sato1@gmail.com>
2014-11-24 16:10:08 +0000

    fix

--- vagrant/centos/setup.sh
+++ vagrant/centos/setup.sh
@@ -5,10 +5,11 @@ yum update -y
 yum install -y \
     bzip2-devel cyrus-sasl-devel libcurl-devel libevent-devel libjpeg libjpeg-devel libpng libpng-devel libxslt-devel make ncurses-devel openssl openssl-devel perl-devel readline-devel rsyslog sqlite-devel tk-devel zlib-devel \
     python-devel python-libs python-passlib python-pip python-setuptools \
-    base-completion \
+    bash-completion \
     docker-io
 
+# for docker
 chkconfig docker on
-yum docker start
+service docker start
 
 pip install fig
